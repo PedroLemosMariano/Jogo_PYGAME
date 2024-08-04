47015302c2283d987ba40daeabfa3a1e177c3e94
PedroLemosMariano <pedro.lemosmariano@gmail.com>
2024-08-04 08:57:35 -0300

    Add files via upload

---
+++ TRABALHO 2 DE O.O/C_PERS_JOGAVEL.py
@@ -0,0 +1,59 @@
+import pygame
+from VAR_GLOBAL import *
+from CA_CARGAS import *
+
+class PERS_JOGAVEL:
+    def __init__(self, x, y, size, spd, cor):
+        self.x = x
+        self.y = y
+        self.size = size
+        self.spd = spd
+        self.cor = cor
+        self.lastkey = 1
+        self.vida = 100
+
+    def update(self):
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_w]:
+            self.lastkey = 0
+            self.y -= self.spd
+
+        if keys[pygame.K_s]:
+            self.lastkey = 1
+            self.y += self.spd
+
+        if keys[pygame.K_a]:
+            self.lastkey = 2
+            self.x -= self.spd
+
+        if keys[pygame.K_d]:
+            self.lastkey = 3
+            self.x += self.spd
+    
+    def draw(self):
+        if self.lastkey == 0:
+            tela.blit(Manoel_costa, (self.x - 70, self.y - 70))
+        elif self.lastkey == 1:
+            tela.blit(Manoel_frente, (self.x - 70, self.y - 70))
+        elif self.lastkey == 2:
+            tela.blit(Manoel_ladoE, (self.x - 70, self.y - 70))
+        elif self.lastkey == 3:
+            tela.blit(Manoel_ladoD, (self.x - 70, self.y - 70))
+
+    def barravida(self):
+        largura_total = 510
+        largura_atual = self.vida * 5
+        pygame.draw.rect(tela, (0, 0, 0), (((tela_x / 2 - 10) - largura_total / 2 + 20), 10, largura_total, 26))
+        pygame.draw.rect(tela, (255, 0, 0), (((tela_x / 2 - 5) - largura_total / 2 + 20), 16, largura_atual, 14))
+
+    def bordas(self):
+        if self.y < self.size + 2:
+            self.y = self.size + 2
+        if self.x < self.size + 2:
+            self.x = self.size + 2
+        if self.y > tela_y - self.size - 2:
+            self.y = tela_y - self.size - 2
+        if self.x > tela_x - self.size - 2:
+            self.x = tela_x - self.size - 2
+
+personagem1 = PERS_JOGAVEL(tela_x / 2, tela_y / 2, 25, 5, (0, 255, 0))
